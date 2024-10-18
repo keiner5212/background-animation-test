cbf15d6f72b9816e8141f7834d6d306113a868ae
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-10-18 15:48:25 -0500

    fix: distance opacity fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,10 @@
 
 export const NUMBER_OF_POINTS = 150
-export const FPS = 40
+export const FPS = 50
 export const INITIAL_VEL_MULIPLIER = 100
 export const MIN_SIZE_POINTS = 5
 export const MAX_SIZE_POINTS = 10
 export const POINTS_BOUNCE = true
-export const POINTS_COLOR = "#ffffff3d"
-export const POINTS_HOVER_COLOR = "#fff"
+export const POINTS_COLOR = "#fff"
+export const POINTS_HOVER_COLOR = "#99f"
 export const HOVER_VELOCITY_MULTIPLIER=1.08
--- utils/drawer.js
+++ utils/drawer.js
@@ -1,8 +1,8 @@
-
 export function DrawPoints(points, ctx) {
 	for (const point of points) {
+		const { r, g, b } = hexToRgb(point.color);
 		ctx.beginPath();
-        ctx.fillStyle = point.color;
+        ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 0.6)`;
 		ctx.arc(point.x, point.y, point.size, 0, 2 * Math.PI);
 		ctx.fill();
 		ctx.closePath();
@@ -10,30 +10,58 @@ export function DrawPoints(points, ctx) {
 }
 
 export function DrawLines(points, ctx) {
-	let nearPoints = [];
-	for (const point of points) {
-		nearPoints = [];
-		for (const nearPoint of points) {
-			if (point !== nearPoint) {
-				if (
-					Math.abs(point.x - nearPoint.x) < 100 &&
-					Math.abs(point.y - nearPoint.y) < 100
-				) {
-					nearPoints.push(nearPoint);
-				}
-			}
-		}
-		for (const nearPoint of nearPoints) {
+    let nearPoints = [];
+    for (const point of points) {
+        nearPoints = [];
+
+        for (const nearPoint of points) {
+            if (point !== nearPoint) {
+                const distanceX = point.x - nearPoint.x;
+                const distanceY = point.y - nearPoint.y;
+                
+                const distanceSquared = distanceX * distanceX + distanceY * distanceY;
+                if (distanceSquared < 10000) {
+                    nearPoints.push(nearPoint);
+                }
+            }
+        }
+
+        const { r, g, b } = hexToRgb(point.color);
+
+        for (const nearPoint of nearPoints) {
+            const distanceX = point.x - nearPoint.x;
+            const distanceY = point.y - nearPoint.y;
+            const distanceSquared = distanceX * distanceX + distanceY * distanceY;
+            
+            const distance = Math.sqrt(distanceSquared); 
+            const opacity = Math.max(0.05, 1 - (distance / 170)); 
+
             ctx.beginPath();
-            ctx.strokeStyle = point.color;
-			ctx.moveTo(point.x, point.y);
-			ctx.lineTo(nearPoint.x, nearPoint.y);
-			ctx.stroke();
-			ctx.closePath();
-		}
-	}
+            ctx.strokeStyle = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+            ctx.moveTo(point.x, point.y);
+            ctx.lineTo(nearPoint.x, nearPoint.y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
 }
+
 
 export function clearCanvas(ctx) {
 	ctx.clearRect(0, 0, innerWidth, innerHeight);
+}
+
+function hexToRgb(hex) {
+    hex = hex.replace('#', '');
+
+    if (hex.length === 3) {
+        hex = hex.split('').map(c => c + c).join('');
+    }
+
+    let bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+
+    return { r, g, b };
 }
